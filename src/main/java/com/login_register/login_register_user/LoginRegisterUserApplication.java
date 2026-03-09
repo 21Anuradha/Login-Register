92ebd499ea2701e9ddece4079e4d1e5c1f459ea2
Anuradha <2anuradhasingh1@gmail.com>
2026-03-09 15:51:51 +0530

    Start the project

---
+++ src/main/java/com/login_register/login_register_user/LoginRegisterUserApplication.java
@@ -0,0 +1,13 @@
+package com.login_register.login_register_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginRegisterUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginRegisterUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/login_register/login_register_user/LoginRegisterUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.login_register.login_register_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginRegisterUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
